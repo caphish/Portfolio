be165df7e213255db53a82018882c888e4342825
Hiteswar Ram <162137954+caphish@users.noreply.github.com>
2024-07-14 12:42:15 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let menu= document.querySelector(".menu-icon");
+let navbar= document.querySelector(".navbar");
+
+menu.onclick =() =>{
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+};
+
+window.onscroll =() =>{
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu");
+};
+
+const animate= ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay: '400'
+  });
+
+  animate.reveal('nav');
+  animate.reveal('.header-image, .card-1, .con-1 , .con-3',{origin:"left"});
+  animate.reveal('.header-content, .card-2 , .con-2', {origin:"right"});
+  animate.reveal('.about-header, .about-subheader, .skills-header , .skills-grid , .skillset, .project-header, .project-subheader,.resume,.contact-header, .contact-upper ,  .timeline', {interval:100});
+  animate.reveal('.footer-content, .socials', {origin:"bottom"});
